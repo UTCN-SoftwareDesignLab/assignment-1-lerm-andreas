26bdc0b4c7168ee5d168d19aa7bab86907e1597b
lerm-andreas <lerm.andreas@gmail.com>
2018-04-01 23:37:48 +0200

    no message

--- project1/src/main/java/database/Constants.java
+++ project1/src/main/java/database/Constants.java
@@ -12,7 +12,6 @@ public class Constants {
 
     public static class Schemas {
         public static final String TEST = "test_bank";
-       // public static final String PRODUCTION = "bank1";
        public static final String PRODUCTION = "test_bank1";
 
 
@@ -20,9 +19,7 @@ public class Constants {
     }
 
     public static class Tables {
-       // public static final String BOOK = "account";
-        //public static final String ACCOUNT = "account";
-       public static final String BOOK = "book";
+
         public static final String CLIENT = "client";
         public static final String ACCOUNT = "account";
         public static final String USER = "user";
@@ -31,7 +28,7 @@ public class Constants {
         public static final String ROLE_RIGHT = "role_right";
         public static final String USER_ROLE = "user_role";
 
-        public static final String[] ORDERED_TABLES_FOR_CREATION = new String[]{USER, ROLE, RIGHT, ROLE_RIGHT, USER_ROLE,BOOK, CLIENT, ACCOUNT};
+        public static final String[] ORDERED_TABLES_FOR_CREATION = new String[]{USER, ROLE, RIGHT, ROLE_RIGHT, USER_ROLE, CLIENT, ACCOUNT};
     }
 
     public static class Roles {
@@ -59,20 +56,6 @@ public class Constants {
         public static final String PROCESS_BILLS = "process_bills";
 
 
-
-        /*public static final String CREATE_USER = "create_user";
-        public static final String DELETE_USER = "delete_user";
-        public static final String UPDATE_USER = "update_user";
-
-        public static final String CREATE_BOOK = "create_book";
-        public static final String DELETE_BOOK = "delete_book";
-        public static final String UPDATE_BOOK = "update_book";
-
-        public static final String SELL_BOOK = "sell_book";
-        public static final String BUY_BOOK = "buy_book";
-        public static final String RETURN_BOOK = "return_book";
-        */
-        //public static final String[] RIGHTS = new String[]{CREATE_USER, DELETE_USER, UPDATE_USER, CREATE_BOOK, DELETE_BOOK, UPDATE_BOOK, SELL_BOOK, BUY_BOOK, RETURN_BOOK};
         public static final String [] RIGHTS = new String[] {CREATE_EMPLOYEE, RETRIEVE_EMPLOYEE, UPDATE_EMPLOYEE,DELETE_EMPLOYEE, GENERATE_REPORTS,
                 ADD_CLIENT,UPDATE_CLIENT, VIEW_CLIENT, CREATE_ACCOUNT,UPDATE_ACCOUNT, DELETE_ACCOUNT, VIEW_ACCOUNT,TRANSFER_MONEY,PROCESS_BILLS };
 
@@ -86,9 +69,6 @@ public class Constants {
         ROLES_RIGHTS.get(ADMINISTRATOR).addAll(Arrays.asList(RIGHTS));
 
         ROLES_RIGHTS.get(EMPLOYEE).addAll(Arrays.asList(ADD_CLIENT,UPDATE_CLIENT, VIEW_CLIENT, CREATE_ACCOUNT,UPDATE_ACCOUNT, DELETE_ACCOUNT, VIEW_ACCOUNT,TRANSFER_MONEY,PROCESS_BILLS));
-
-
-      //  ROLES_RIGHTS.get(CUSTOMER).addAll(Arrays.asList(SELL_BOOK, BUY_BOOK, RETURN_BOOK));
 
         return ROLES_RIGHTS;
     }
